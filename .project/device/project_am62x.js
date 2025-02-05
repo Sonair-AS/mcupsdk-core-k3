d21bba01272c0c775f929ec7b4de8f906db073d9
Vaibhav Reddy <vaibhav.reddy@ti.com>
2025-02-05 18:57:10 +0530

    am62x: coremark-pro benchmark: Add sha test workload benchmark
     -Add sha workload which implements cryptographic sha-256
      hashing algorithm
    
     -Add build infra and update generated files
    
    Fixes: SITSW-6311
    
    Signed-off-by: Vaibhav Reddy <vaibhav.reddy@ti.com>

--- .project/device/project_am62x.js
+++ .project/device/project_am62x.js
@@ -52,6 +52,7 @@ const example_file_list = [
     "examples/benchmarks/coremark_pro_benchmark/nnet_test/.project/project.js",
     "examples/benchmarks/coremark_pro_benchmark/parser-125k/.project/project.js",
     "examples/benchmarks/coremark_pro_benchmark/radix2-big-64k/.project/project.js",
+    "examples/benchmarks/coremark_pro_benchmark/sha-test/.project/project.js",
     "examples/benchmarks/dhrystone_benchmark/.project/project.js",
     "examples/benchmarks/interrupt_latency_benchmark/.project/project.js",
     "examples/drivers/boot/sbl_emmc_linux_multistage/sbl_emmc_linux_stage1/.project/project.js",
