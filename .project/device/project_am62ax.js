e9be62c72e192c773ddc7350f3eab748e5f0c4aa
raj <r-panganamala@ti.com>
2025-02-25 15:31:39 +0530

    am62ax:  sdl: add ecc example for i-cache and d-cache
    Add ecc example for r5f cache memories
    
    Fixes: SITREQ-4552
    
    Signed-off-by: Raj Panganamala <r-panganamala@ti.com>

--- .project/device/project_am62ax.js
+++ .project/device/project_am62ax.js
@@ -219,6 +219,7 @@ const example_file_list = [
     // SDL Examples and tests
     "examples/sdl/dcc/dcc_modes/.project/project.js",
     "examples/sdl/ecc/.project/project.js",
+    "examples/sdl/ecc_r5f_cache/.project/project.js",
     "examples/sdl/esm/mcu_esm0/.project/project.js",
     "examples/sdl/mcrc/mcrc_full_cpu/.project/project.js",
     "examples/sdl/mcrc/mcrc_auto/.project/project.js",
